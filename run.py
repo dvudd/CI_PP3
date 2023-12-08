ddb7cdd8c243006f35d69e9beddb887c14394eeb
David Eriksson <david.eriksson@lundkroken.se>
2023-12-08 11:03:43 +0100

    feat: Displays two rows, keep cursor in the middle

--- run.py
+++ run.py
@@ -37,12 +37,12 @@ def speed_test(stdscr):
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     stdscr.clear()
 
-    # Generate three rows of random words
-    rows = [get_random_string() for _ in range(3)]
+    # Generate two rows of random words
+    rows = [""] + [get_random_string() for _ in range(2)]
     # Initialize a list to store the user's input
     user_input = ["" for _ in range(3)]
-
-    pos = 0
+    # Set the starting position in them middle row
+    pos = 1
     while True:
         # Display rows
         for i in range(3):
@@ -50,8 +50,9 @@ def speed_test(stdscr):
 
         # Display user input, color green if correct or red if incorrect
         for i in range(3):
+            stdscr.addstr(i, 0, rows[i])
             for j in range(len(user_input[i])):
-                correct = rows[i][j]
+                correct = rows[i][j] if j < len(rows[i]) else " "
                 current_string = user_input[i][j]
                 color_pair = 1 if current_string == correct else 2
                 attribute = (
@@ -72,16 +73,28 @@ def speed_test(stdscr):
             if len(user_input[pos]) > 0:
                 user_input[pos] = user_input[pos][:-1]
             elif pos > 0:
+                # Move to the previous row
                 pos -= 1
-                user_input[pos] = user_input[pos][:-1]
         # Check if input is a printable character
         elif len(key) == 1 and key.isprintable():
             user_input[pos] += key
+            if len(user_input[pos]) == len(rows[pos]):
+                # Once row1 is completed, move back to row2
+                if pos == 0:
+                    pos = 1
 
-        # Move to next row if current row is completed
-        if len(user_input[pos]) == len(rows[pos]):
-            # Ensure pos does not exceed 2
-            pos = min(pos + 1, 2)
+        # Shift rows up and load a new row
+        if len(user_input[1]) == len(rows[1]):
+            # Move row2 to row1, row3 to row2
+            rows[:2] = rows[1:]
+            # Generate a new row at row3
+            rows[2] = get_random_string()
+            # Shift user_input
+            user_input[:2] = user_input[1:]
+            # New empty string for row3
+            user_input[2] = ""
+            # Keep typing on row2
+            pos = 1
 
         # Refresh the screen
         stdscr.refresh()
