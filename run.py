11bff63edabc2be2662da27d27fd1c17ed4c3b9f
David Eriksson <david.eriksson@lundkroken.se>
2023-12-11 11:16:02 +0100

    feat: Adds timer function

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import curses
 import random
+import time
 
 
 def get_random_string(length=10):
@@ -16,15 +17,19 @@ def get_random_string(length=10):
     return " ".join(random_words) + " "
 
 
-def speed_test(stdscr):
+def speed_test(stdscr, timer_length=30):
     # Initialize curses
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     stdscr.clear()
+    stdscr.nodelay(True)
 
     # Initialize counters and cursor positions
     correct_chars = 0
     incorrect_chars = 0
+    start_time = None
+    end_time = None
 
     # Initialize cursor position, start in the middle row
     pos_y, pos_x = 1, 0
@@ -60,55 +65,67 @@ def speed_test(stdscr):
         stdscr.move(cursor_y, cursor_x)
 
         # Listen for keyboard presses
-        key = stdscr.getkey()
+        try:
+            key = stdscr.getkey()
+        except curses.error:
+            key = None
 
-        # Backspace handling
-        if key in ["KEY_BACKSPACE", "\b", "\x7f"]:
-            if pos_x > 0:
-                pos_x -= 1
-                last_char_correct = user_input[pos_y][pos_x] == rows[pos_y][pos_x]
-                user_input[pos_y] = user_input[pos_y][:pos_x] + user_input[pos_y][pos_x+1:]
-                if last_char_correct:
-                    correct_chars -= 1
-                else:
-                    incorrect_chars -= 1
-            elif pos_y > 0 and user_input[pos_y - 1]:
-                pos_y -= 1
-                pos_x = len(user_input[pos_y])
-       
-        # Printable character handling
-        elif len(key) == 1 and key.isprintable():
-            if pos_x < len(rows[pos_y]):
-                user_input[pos_y] = user_input[pos_y][:pos_x] + key + user_input[pos_y][pos_x:]
-                is_correct = key == rows[pos_y][pos_x]
-                if is_correct:
-                    correct_chars += 1
-                else:
-                    incorrect_chars += 1
-                pos_x += 1
+        if key:
+            # Start the timer once user presses a key
+            if start_time is None:
+                start_time = time.time()
+                end_time = start_time + timer_length
 
-                # Move cursor back to middle row after completing the top row
-                if pos_y == 0 and pos_x == len(rows[pos_y]):
-                    pos_y = 1
+            # Backspace handling
+            if key in ["KEY_BACKSPACE", "\b", "\x7f"]:
+                if pos_x > 0:
+                    pos_x -= 1
+                    last_char_correct = user_input[pos_y][pos_x] == rows[pos_y][pos_x]
+                    user_input[pos_y] = user_input[pos_y][:pos_x] + user_input[pos_y][pos_x+1:]
+                    if last_char_correct:
+                        correct_chars -= 1
+                    else:
+                        incorrect_chars -= 1
+                elif pos_y > 0 and user_input[pos_y - 1]:
+                    pos_y -= 1
                     pos_x = len(user_input[pos_y])
+        
+            # Printable character handling
+            elif len(key) == 1 and key.isprintable():
+                if pos_x < len(rows[pos_y]):
+                    user_input[pos_y] = user_input[pos_y][:pos_x] + key + user_input[pos_y][pos_x:]
+                    is_correct = key == rows[pos_y][pos_x]
+                    if is_correct:
+                        correct_chars += 1
+                    else:
+                        incorrect_chars += 1
+                    pos_x += 1
 
-        # Row completion and shifting logic
-        if pos_y == 1 and pos_x >= len(rows[pos_y]):
-            rows.pop(0)
-            rows.append(get_random_string())
-            user_input.pop(0)
-            user_input.append("")
-            pos_x = 0
+                    # Move cursor back to middle row after completing the top row
+                    if pos_y == 0 and pos_x == len(rows[pos_y]):
+                        pos_y = 1
+                        pos_x = len(user_input[pos_y])
 
-        # REMOVE THIS, ONLY FOR TESTING
-        stdscr.clear()
-        stdscr.addstr(3, 0, str(correct_chars), curses.color_pair(1))
-        stdscr.addstr(4, 0, str(incorrect_chars), curses.color_pair(2))
-        stdscr.addstr(5, 0, str(max_x), curses.color_pair(2))
-        stdscr.addstr(6, 0, str(max_y), curses.color_pair(2))
+            # Row completion and shifting logic
+            if pos_y == 1 and pos_x >= len(rows[pos_y]):
+                rows.pop(0)
+                rows.append(get_random_string())
+                user_input.pop(0)
+                user_input.append("")
+                pos_x = 0
 
-        # Refresh the screen
-        stdscr.refresh()
+        # Display the remaing time
+        if start_time:
+            remaining_time = max(int(end_time - time.time()), 0)
+        else:
+            remaining_time = timer_length
+        stdscr.addstr(y_position - 5,  max_x // 2 - 30, str(f"{remaining_time}s"), curses.color_pair(3))
+
+        # Check if the time is up
+        if remaining_time <= 0:
+            break
+
+    return correct_chars, incorrect_chars
 
 
 def main():
